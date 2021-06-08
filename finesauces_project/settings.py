73c4b11280df6c3db892df8cacc0ab3e65bc7947
Bryan Arredondo <51183579+estradahub@users.noreply.github.com>
2021-06-08 14:40:56 -0500

    Update settings.py

--- finesauces_project/settings.py
+++ finesauces_project/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pxz(#c8$a@c03j7se4jrj)l@8x)3sw&4tm06hm4a1-=a)r32(x'
+SECRET_KEY = 'django-insecure-pxz(#c8$a@cse2003j7se4jrj)l@8x)3sw&4tm06hm4a1-=a)r32(x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
